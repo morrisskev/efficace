26c15359c1302f129f5a259ba170f807d010204e
Rayan <ryanalaz11@gmail.com>
2022-03-08 13:51:30 +0100

    Optimized with weights, better score

--- TP4/main.py
+++ TP4/main.py
@@ -2,7 +2,7 @@
 from fileinput import close
 from glob import glob
 import math
-
+import sys
 
 warehouse_coords = []
 warhouse_products = []
@@ -141,12 +141,39 @@ def best_order(drone_index):
         return priority_orders[0]
 
 
+def best_first_product(drone_index, order_index):
+    # Just get the min weight product of the order
+    min = 999999999
+    min_index = -1
+    for index, item in enumerate(order_items[order_index]):
+        if(product_weights[item] < min):
+            min = product_weights[item]
+            min_index = index
+    return min_index
+
+
+def load_more(warehouse_index, cur_weight, order_index):
+    to_load = []
+    temp_warehouse_products = warhouse_products[warehouse_index].copy()
+    for index, item in enumerate(order_items[order_index]):
+        if(temp_warehouse_products[item] > 0):
+            if cur_weight + product_weights[item] <= max_payload:
+                to_load.append(item)
+                cur_weight += product_weights[item]
+                temp_warehouse_products[item] -= 1
+
+    return to_load
+
+
 def assign_drone(index):
     print("----------------------------------------------------")
     print("Assigning drone " + str(index))
     print("Coords: " + str(drone_coords[index]))
     best_order_index = best_order(index)
-    order_item_index = order_items[best_order_index].pop(0)
+    best_first_product_index = best_first_product(index, best_order_index)
+    order_item_index = order_items[best_order_index].pop(
+        best_first_product_index)
+    cur_drone_weight = product_weights[order_item_index]
     order_quantity[best_order_index] -= 1
     best_warehouse_index = best_warehouse_for_product(index, order_item_index)
     warhouse_products[best_warehouse_index][order_item_index] -= 1
@@ -155,6 +182,17 @@ def assign_drone(index):
         move_drone(index, warehouse_coords[best_warehouse_index]
                    [0], warehouse_coords[best_warehouse_index][1])
     load_product(index, best_warehouse_index, order_item_index)
+    # Check if warehouse has other products from the order
+    to_load = load_more(best_warehouse_index,
+                        cur_drone_weight, best_order_index)
+    print("To load: " + str(to_load))
+    for item in to_load:
+        order_items[best_order_index].remove(item)
+        load_product(index, best_warehouse_index, item)
+        order_quantity[best_order_index] -= 1
+        print("Removing item " + str(item) + " From warehouse")
+        print(warhouse_products[best_warehouse_index])
+        warhouse_products[best_warehouse_index][item] -= 1
     drone_status[index] = distance
     drone_coords[index] = warehouse_coords[best_warehouse_index]
     print("Drone is at Warehouse. Now moving to delivery")
@@ -164,6 +202,8 @@ def assign_drone(index):
     drone_status[index] += distance
     drone_coords[index] = order_coords[best_order_index]
     deliver_product(index, best_order_index, order_item_index)
+    for item in to_load:
+        deliver_product(index, best_order_index, item)
     print("Drone is at order coordinates. Delivered product")
     print("----------------------------------------------------\n")
     return 0
@@ -185,6 +225,7 @@ def solve():
 
 
 with open("busy_day.in") as f:
+    # with open(str(sys.argv[1])) as f:
     lines = f.readlines()
     # Format : First line : rows, columns, drones, turns, max payload
     rows, columns, drones, turns, max_payload = [
