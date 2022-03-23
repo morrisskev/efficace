fe07dcabecd6611475aafadf64f3ac3238d0466d
Rayan <ryanalaz11@gmail.com>
2022-03-23 16:04:15 +0100

    Final

--- TP6/main.py
+++ TP6/main.py
@@ -23,6 +23,7 @@ def compute_seq():
     for n in seq:
         notes_played[n-1] += 1
 
+
 def parse(path):
     global nb_notes
     global seq_len
@@ -41,37 +42,31 @@ def parse(path):
         for i in range(0, len(freq_raw)):
             freq.append(freq_raw[i] / eq_multiplier)
 
-        seq = [int(x) for x in data[2].split(" ")]
+        seq = [] if data[2] == "" else [int(x) for x in data[2].split(" ")]
 
         compute_seq()
 
         total_notes_played = seq_len
 
-        print("nb_notes: " + str(nb_notes))
-        print("seq_len: " + str(seq_len))
-        print("freq raw: " + str(freq_raw))
-        print("freq: " + str(freq))
-        print("seq: " + str(seq))
-        print("eq multiplier: " + str(eq_multiplier))
-        print("notes played: " + str(notes_played))
         f.close()
 
-def testonleft(notes_played,total_notes_played,freq,nb_notes,i):
-    for j in range (0,nb_notes):
-        if not j==i and (not (total_notes_played+1)*freq[j]-1<notes_played[j] ):
-                return False
+
+def testonleft(notes_played, total_notes_played, freq, nb_notes, i):
+    for j in range(0, nb_notes):
+        if not j == i and (not (total_notes_played+1)*freq[j]-1 < notes_played[j]):
+            return False
     return True
 
 
-def bestNote(notes_played,total_notes_played,freq,nb_notes):
-    min=10000
-    indice=-1
-    for i in range (0,nb_notes):
-        r=notes_played[i]-(total_notes_played*freq[i]-1)
-        if r>0 and min>r:
-            if (notes_played[i]+1<(total_notes_played+1)*freq[i]+1) and testonleft(notes_played,total_notes_played,freq,nb_notes,i):
-                min=r
-                indice=i
+def bestNote(notes_played, total_notes_played, freq, nb_notes):
+    min = 10000
+    indice = -1
+    for i in range(0, nb_notes):
+        r = notes_played[i]-(total_notes_played*freq[i]-1)
+        if r > 0 and min > r:
+            if (notes_played[i]+1 < (total_notes_played+1)*freq[i]+1) and testonleft(notes_played, total_notes_played, freq, nb_notes, i):
+                min = r
+                indice = i
     return indice
 
 
@@ -85,32 +80,49 @@ def solve():
     global total_notes_played
     global notes_played
 
-    compt=0
-    sequence_add=[]
-    while(compt<100):   
-        indice=bestNote(notes_played,total_notes_played,freq,nb_notes)
-        if(indice==-1):
+    compt = 0
+    sequence_add = []
+    while(compt < 100):
+        indice = bestNote(notes_played, total_notes_played, freq, nb_notes)
+        if(indice == -1):
             break
         else:
-            notes_played[indice]+=1
-            total_notes_played+=1
-            compt=compt+1
+            notes_played[indice] += 1
+            total_notes_played += 1
+            compt = compt+1
             sequence_add.append(indice+1)
 
-    if (compt>=100):
-        print("sequence total: " +str(seq+sequence_add))
+    if (compt >= 100):
         print("resultat="+str(compt))
         print("infini")
-    else :
-        print("sequence total: " +str(seq+sequence_add))
+    else:
         print("resultat="+str(compt))
-
 
 
 def main():
-    path=sys.argv[1]
-    parse(path)
-    solve()
+    global nb_notes
+    global seq_len
+    global freq
+    global freq_raw
+    global seq
+    global eq_multiplier
+    global total_notes_played
+    global notes_played
+    for i in range(3, 84):
+        path = "0" + str(i) + ".in" if i < 10 else str(i) + ".in"
+        print("TESTING FILE " + path)
+        print("------------------")
+        parse(path)
+        solve()
+        nb_notes = 0
+        seq_len = 0
+        freq = []
+        freq_raw = []
+        seq = []
+        eq_multiplier = 0
+        total_notes_played = 0
+        notes_played = []
+        print("------------------\n")
 
 
 if __name__ == "__main__":
